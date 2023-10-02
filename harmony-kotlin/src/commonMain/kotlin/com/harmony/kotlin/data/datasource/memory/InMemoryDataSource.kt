250826951b0b5cd41af6af8aa66c8bf85409cf20
Fran Montiel <furanu@gmail.com>
2023-10-03 00:05:48 +0200

    KMM :: Internal :: Make InMemoryDataSource thread-safe

--- harmony-kotlin/src/commonMain/kotlin/com/harmony/kotlin/data/datasource/memory/InMemoryDataSource.kt
+++ harmony-kotlin/src/commonMain/kotlin/com/harmony/kotlin/data/datasource/memory/InMemoryDataSource.kt
@@ -8,40 +8,53 @@ import com.harmony.kotlin.data.query.KeyQuery
 import com.harmony.kotlin.data.query.Query
 import com.harmony.kotlin.error.DataNotFoundException
 import com.harmony.kotlin.error.notSupportedQuery
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class InMemoryDataSource<V> : GetDataSource<V>, PutDataSource<V>, DeleteDataSource {
 
   private val objects: MutableMap<String, V> = mutableMapOf()
+  private val mutex = Mutex()
 
   override suspend fun get(query: Query): V =
-    when (query) {
-      is KeyQuery -> {
-        objects[query.key].run {
-          this ?: throw DataNotFoundException()
+    mutex.withLock {
+      when (query) {
+        is KeyQuery -> {
+          objects[query.key].run {
+            this ?: throw DataNotFoundException()
+          }
         }
+
+        else -> notSupportedQuery()
       }
-      else -> notSupportedQuery()
     }
 
   override suspend fun put(query: Query, value: V?): V =
-    when (query) {
-      is KeyQuery -> {
-        value?.let {
-          objects.put(query.key, value).run { value }
-        } ?: throw IllegalArgumentException("InMemoryDataSource: value must be not null")
+    mutex.withLock {
+      when (query) {
+        is KeyQuery -> {
+          value?.let {
+            objects.put(query.key, value).run { value }
+          } ?: throw IllegalArgumentException("InMemoryDataSource: value must be not null")
+        }
+
+        else -> notSupportedQuery()
       }
-      else -> notSupportedQuery()
     }
 
   override suspend fun delete(query: Query) {
-    when (query) {
-      is AllObjectsQuery -> {
-        objects.clear()
+    mutex.withLock {
+      when (query) {
+        is AllObjectsQuery -> {
+          objects.clear()
+        }
+
+        is KeyQuery -> {
+          clearAll(query.key)
+        }
+
+        else -> notSupportedQuery()
       }
-      is KeyQuery -> {
-        clearAll(query.key)
-      }
-      else -> notSupportedQuery()
     }
   }
 
